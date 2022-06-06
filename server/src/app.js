500784e7c6557e086ac017867ad192aaaf887fbc
Spencer Pequegnat <spencer.peq@gmail.com>
2022-06-05 19:12:23 -0700

    serving frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -10,6 +11,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
